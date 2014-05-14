ed05987cc1902240c30bdff8ca5d1ac55b15b446
starboychina <starboychina@cf12fd66-4842-4c7e-bb0a-58ecfda43f04>
2014-05-14 15:12:38 +0000

    百度统计
    git-svn-id: http://bbs.fendoujp.com/svn/newfendoujp/trunk@8 cf12fd66-4842-4c7e-bb0a-58ecfda43f04

--- cn/tpl/foot.php
+++ cn/tpl/foot.php
@@ -23,5 +23,10 @@
 </div>
 <script src="/cn/js/index.js"></script> 
 <script src="/cn/js/desSlideshow.js"></script>
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fa1ec215a9bbdba8041ff74b82d27231d' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
 </body>
 </html>

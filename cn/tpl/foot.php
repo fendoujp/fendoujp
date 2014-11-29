509146399be2df0745f99ece163219482d9c8d0e
starboychina <starboychina@cf12fd66-4842-4c7e-bb0a-58ecfda43f04>
2014-11-29 14:04:05 +0000

    git-svn-id: http://bbs.fendoujp.com/svn/newfendoujp/trunk@19 cf12fd66-4842-4c7e-bb0a-58ecfda43f04

--- cn/tpl/foot.php
+++ cn/tpl/foot.php
@@ -62,16 +62,6 @@ function wwdresize(){
 
 <script type="text/javascript">
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F396c562776f414444aec2720f9a2f21d' type='text/javascript'%3E%3C/script%3E"));
-</script>
-
-
-<script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fa1ec215a9bbdba8041ff74b82d27231d' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F2905d3a05e29317306cd014d7baedbda' type='text/javascript'%3E%3C/script%3E"));
 </script>
 </body>
